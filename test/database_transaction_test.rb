1d41c2c7bf413ed598aac75d744365885a8c12e6
rvernagus <r.vernagus@gmail.com>
2009-04-07 12:39:40 -0400

    Add first transaction test.

--- test/database_transaction_test.rb
+++ test/database_transaction_test.rb
@@ -1,1 +1,13 @@
-#!/usr/bin/env ruby
+require "database_test_fixture"
+
+class DatabaseTransactionTest  < Test::Unit::TestCase
+  include DatabaseTestFixture
+  
+  def test_begins_a_transaction
+    assert !@provider.connection.transaction_started?
+    
+    @db.transaction
+    
+    assert @provider.connection.transaction_started?
+  end
+end
