cd46130fe4881cf2413f4d4d805b9c5c2dce1b47
rvernagus <r.vernagus@gmail.com>
2009-04-07 15:04:24 -0400

    Add SqlServer transaction test.

---
+++ sql_client_test/transaction_test.rb
@@ -0,0 +1,19 @@
+require "test/unit"
+require "lib/irdb"
+require "System.Data"
+
+class SqlTransactionTest < Test::Unit::TestCase
+  def setup
+    provider_factory = IRDb::DbProviderFactory.new
+    provider = provider_factory.get_provider("System.Data.SqlClient")
+    cstr = "Data Source=.\\sqlexpress;Initial Catalog=IRDbTest;Integrated Security=True;"
+
+    @db = IRDb::Database.new(provider, cstr)
+  end
+  
+  def test_created_successfully
+    @db.transaction do |t|
+      assert_not_nil t
+    end
+  end
+end
