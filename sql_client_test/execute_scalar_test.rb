afa76c7f3365e0159a6bcc52ce50548df0fcfc28
rvernagus <r.vernagus@gmail.com>
2009-04-08 15:23:51 -0400

    Add test using parameter.

---
+++ sql_client_test/execute_scalar_test.rb
@@ -0,0 +1,27 @@
+require "test/unit"
+require "lib/irdb"
+require "System.Data"
+
+class SqlExecuteScalarTest < Test::Unit::TestCase
+  def setup
+    provider_factory = IRDb::DbProviderFactory.new
+    provider = provider_factory.get_provider("System.Data.SqlClient")
+    cstr = "Data Source=.\\sqlexpress;Initial Catalog=IRDbTest;Integrated Security=True;"
+
+    @db = IRDb::Database.new(provider, cstr)
+  end
+  
+  def test_returns_value
+    result = @db.execute_scalar("SELECT 'Death Knight'")
+    
+    assert_equal "Death Knight", result
+  end
+  
+  def test_using_a_parameter
+    result = @db.execute_scalar("SELECT @param") do |cmd|
+      @db.add_parameter(cmd, :name => "param", :value => "value".to_string)
+    end
+    
+    assert_equal "value", result
+  end
+end
