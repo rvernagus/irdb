898c9cb5b6ea5d702e5c150db9ec9319d15a2cfd
rvernagus <r.vernagus@gmail.com>
2009-04-10 08:32:34 -0400

    Move command tests to specs.

---
+++ spec/core/command_spec.rb
@@ -0,0 +1,43 @@
+describe Database, "when executing the command method" do
+  before :each do
+    @provider = FakeProvider.new
+    @db = Database.new(@provider, "connection string")
+    @conn = @db.instance_variable_get("@conn")
+  end
+  
+  it "should yield the expected command object" do
+    was_yielded = false
+    @db.command("command text") do |cmd|
+      was_yielded = true
+      cmd.should == @provider.command
+    end
+    
+    was_yielded.should.be_true
+  end
+  
+  it "should set the connection property of the command" do
+    @db.command("command text") do |cmd|
+      cmd.connection.should == @provider.connection
+    end
+  end
+  
+  it "should set the command_text property of the command" do
+    @db.command("command text") do |cmd|
+      cmd.command_text.should == "command text"
+    end
+  end
+  
+  it "should not set the transaction property if there is not one current" do
+    @db.command("command text") do |cmd|
+      cmd.transaction.should.be_nil
+    end
+  end
+  
+  it "should set the transaction property if there is one current" do
+    @db.transaction do |t|
+        @db.command("command text") do |cmd|
+          cmd.transaction.should == t
+        end
+    end
+  end
+end
