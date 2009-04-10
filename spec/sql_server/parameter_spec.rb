fe06b7c2c49cfc257613c04ed5fc89465bc8beb2
rvernagus <r.vernagus@gmail.com>
2009-04-10 15:09:43 -0400

    Move SQL Server parameter tests to spec.

---
+++ spec/sql_server/parameter_spec.rb
@@ -0,0 +1,15 @@
+describe Database, "commands on SQL Server" do
+  extend SqlServerHelper
+  
+  before :each do
+    @db = get_database
+  end
+  
+  it "should set the value to expected" do
+    result = @db.execute_scalar("SELECT @param") do |cmd|
+      @db.add_parameter(cmd, :name => "param", :value => "value".to_string)
+    end
+    
+    result.should == "value"
+  end
+end
