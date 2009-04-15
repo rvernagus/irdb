df655982fe23d8c493e769d113fcc2d37028811c
Ray Vernagus <r.vernagus@gmail.com>
2009-04-15 10:34:58 -0400

    SQL Server specs now drop the test table and data for each spec.

--- spec/sql_server/spec_helper.rb
+++ spec/sql_server/spec_helper.rb
@@ -3,15 +3,20 @@ lib_path = File.expand_path(dir_path + "/../../lib")
 $:.unshift lib_path unless $:.include? lib_path
 
 require "irdb"
+require "yaml"
 include IRDb
+
+$config = YAML.load_file(dir_path + "/config.yaml")
 
 module SqlServerHelper
   def get_database
     provider_factory = DbProviderFactory.new
-    provider = provider_factory.create_provider("System.Data.SqlClient")
+    provider = provider_factory.create_provider($config[:provider])
+    db = Database.new(provider, $config[:cstr])
     
-    cstr = "Data Source=.\\sqlexpress;Initial Catalog=IRDbTest;Integrated Security=True;"
-  
-    Database.new(provider, cstr)
+    $config[:setup].each do |sql|
+      db.execute_non_query(sql) rescue nil
+    end
+    db
   end
 end
--- spec/sql_server/transaction_spec.rb
+++ spec/sql_server/transaction_spec.rb
@@ -12,4 +12,34 @@ describe Database, "transactions on SQL Server" do
       end
     end
   end
+  
+  it "should commit" do
+    @db.connection do |c|
+      @db.transaction do |t|
+        @db.command("DELETE FROM characters") do |cmd|
+          cmd.execute_scalar
+        end
+      end
+    end
+    
+    result = @db.execute_scalar("SELECT COUNT(*) FROM characters")
+    result.should == 0
+  end
+  
+  it "should rollback on error" do
+    lambda {
+      @db.connection do |c|
+        @db.transaction do |t|
+          @db.command("DELETE FROM characters") do |cmd|
+            cmd.execute_scalar
+          end
+          raise Exception
+        end
+      end
+    }.should raise_error(Exception)
+    
+    
+    result = @db.execute_scalar("SELECT COUNT(*) FROM characters")
+    result.should == 2
+  end
 end
