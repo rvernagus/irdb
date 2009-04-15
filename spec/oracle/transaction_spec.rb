0cf323b6d31e1aed30f7f82b7eb23c6648e53048
Ray Vernagus <r.vernagus@gmail.com>
2009-04-15 10:13:03 -0400

    Add transaction commit and rollback specs.

--- spec/oracle/transaction_spec.rb
+++ spec/oracle/transaction_spec.rb
@@ -12,4 +12,33 @@ describe Database, "transactions on Oracle" do
       end
     end
   end
+  
+  it "should commit" do
+    @db.connection do |c|
+      @db.transaction do |t|
+        @db.command("DELETE FROM characters") do |cmd|
+          cmd.execute_scalar
+        end
+      end
+    end
+    
+    result = @db.execute_scalar("SELECT COUNT(*) FROM characters")
+    result = System::Convert.to_int32(result)
+    result.should == 0
+  end
+  
+  it "should rollback on error" do
+    lambda {
+      @db.connection do |c|
+        @db.transaction do |t|
+          @db.execute_non_query("DELETE FROM characters")
+          raise Exception
+        end
+      end
+    }.should raise_error(Exception)
+    
+    result = @db.execute_scalar("SELECT COUNT(*) FROM characters")
+    result = System::Convert.to_int32(result)
+    result.should == 2
+  end
 end
