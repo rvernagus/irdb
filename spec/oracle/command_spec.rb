1e87e47f20b17aa4a1b09af5861571ab17a5eab6
Ray Vernagus <r.vernagus@gmail.com>
2009-04-16 11:10:06 -0400

    Update Oracle specs per latest changes.

--- spec/oracle/command_spec.rb
+++ spec/oracle/command_spec.rb
@@ -7,42 +7,35 @@ describe Database, "commands on Oracle" do
   
   it "should have expected connection" do
     @db.connection do |c|
-      @db.command("SELECT 'Death Knight' FROM dual") do |cmd|
-        cmd.connection.should == c
-      end
+      cmd = @db.command("SELECT 'Death Knight' FROM dual")
+      cmd.connection.should == c
     end
   end
 
   it "should have open connection" do
     @db.connection do |c|
-      @db.command("SELECT 'Death Knight' FROM dual") do |cmd|
-        cmd.connection.state.should == System::Data::ConnectionState.open
-      end
+      cmd = @db.command("SELECT 'Death Knight' FROM dual")
+      cmd.connection.state.should == System::Data::ConnectionState.open
     end
   end
 
   it "should allow multiple commands within single connection block" do
     @db.connection do |c|
-      @db.command("SELECT 'Death Knight' FROM dual") do |cmd|
-        result = cmd.execute_scalar
-        result.should == "Death Knight"
-      end
+      cmd = @db.command("SELECT 'Death Knight' FROM dual")
+      result = cmd.execute_scalar
+      result.should == "Death Knight"
       
-      @db.command("SELECT 'Shaman' FROM dual") do |cmd|
-        result = cmd.execute_scalar
-        result.should == "Shaman"
-      end
+      cmd = @db.command("SELECT 'Shaman' FROM dual")
+      result = cmd.execute_scalar
+      result.should == "Shaman"
     end
   end
   
   it "should allow command to execute with an active transaction" do
-    @db.connection do |c|
-      @db.transaction do |t|
-        @db.command("SELECT 'Death Knight' FROM dual") do |cmd|
-          result = cmd.execute_scalar
-          result.should == "Death Knight"
-        end
-      end
+    @db.transaction do |t|
+      cmd = @db.command("SELECT 'Death Knight' FROM dual")
+      result = cmd.execute_scalar
+      result.should == "Death Knight"
     end
   end
 end
