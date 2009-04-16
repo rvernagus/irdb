199aaed722234d8edf03167b31edf207141f00d4
Ray Vernagus <r.vernagus@gmail.com>
2009-04-16 13:44:21 -0400

    Add execute_hash spec against table query.

--- spec/oracle/execute_hash_spec.rb
+++ spec/oracle/execute_hash_spec.rb
@@ -5,9 +5,17 @@ describe Database, "execute_hash on Oracle" do
     @db = get_database
   end
   
-  it "should return expected record hash" do
+  it "should return expected record hash from a scalar query" do
     result = @db.execute_hash("SELECT 'value' AS key FROM dual")
     
     result.should == [{"key" => "value"}]
+  end
+  
+  it "should return the expected record hash from a table query" do
+    result = @db.execute_hash("SELECT * FROM characters ORDER BY id")
+    
+    result.length.should == 2
+    result[0]["class"].should == "Death Knight"
+    result[1]["class"].should == "Shaman"
   end
 end
