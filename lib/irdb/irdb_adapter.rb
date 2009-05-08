df6a0443d1e119f4e5326b158fd6ae2a3967ae4f
Ray Vernagus <r.vernagus@gmail.com>
2009-05-07 21:08:33 -0400

    Experimental columns.

--- lib/irdb/irdb_adapter.rb
+++ lib/irdb/irdb_adapter.rb
@@ -7,17 +7,29 @@ module ActiveRecord
       cstr          = config[:cstr] || config[:connection_string]
       
       provider_factory = IRDb::DbProviderFactory.new
-      provider = provider_factory.create_provider(provider_name)
+      #provider = provider_factory.create_provider(provider_name)
+      provider = FakeProvider.new
       db = IRDb::Database.new(provider, cstr)
       ConnectionAdapters::IrdbAdapter.new(db, logger, config)
     end
   end
   
   module ConnectionAdapters
+    class IrdbColumn < Column
+      
+    end
+    
     class IrdbAdapter < AbstractAdapter
       def initialize(connection, logger, config)
         super(connection, logger)
         @config = config
+      end
+      
+      def columns(table_name, name=nil)
+        [IrdbColumn.new("id", nil, :integer),
+         IrdbColumn.new("wow_class", nil, :string),
+         IrdbColumn.new("level", nil, :integer),
+         IrdbColumn.new("slogan", nil, :string)]
       end
     end
   end
