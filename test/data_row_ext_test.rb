4f7ce9fb5a0f6f266686f1b764ce4f4e3809b12c
Ray Vernagus <r.vernagus@gmail.com>
2010-01-11 22:46:55 -0500

    Split DataRow context into sub-contexts.

--- test/data_row_ext_test.rb
+++ test/data_row_ext_test.rb
@@ -3,19 +3,36 @@ require File.join(File.dirname(__FILE__), 'test_helper')
 class DataRowExtensions < Test::Unit::TestCase
   context 'to_hash' do
     setup do
-      table = System::Data::DataTable.new
-      table.columns.add 'col1', String.to_clr_type
-      table.columns.add 'col2', String.to_clr_type
-      
-      @row = table.new_row      
+      @table = System::Data::DataTable.new
     end
     
-    should 'returns hash of column names and values' do
-      @row['col1'] = 'value1'
-      @row['col2'] = 'value2'
-      result = @row.to_hash
+    context 'when table has no columns' do
+      setup do
+        @row = @table.new_row      
+      end
       
-      assert_equal({ 'col1' => 'value1', 'col2' => 'value2' }, result)
+      should 'returns empty hash' do
+        result = @row.to_hash
+        
+        assert_equal({}, result)
+      end
+    end
+    
+    context 'when table has columns' do
+      setup do
+        @table.columns.add 'col1', String.to_clr_type
+        @table.columns.add 'col2', String.to_clr_type
+      
+        @row = @table.new_row      
+      end
+      
+      should 'returns hash of column names and values' do
+        @row['col1'] = 'value1'
+        @row['col2'] = 'value2'
+        result = @row.to_hash
+        
+        assert_equal({ 'col1' => 'value1', 'col2' => 'value2' }, result)
+      end
     end
   end
 end
