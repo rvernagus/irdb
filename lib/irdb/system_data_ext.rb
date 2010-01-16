417809167e54c546c5c0123069dc27418ace20a8
Ray Vernagus <r.vernagus@gmail.com>
2010-01-15 22:23:04 -0500

    Add method_missing to DataRow. Will return value of column if method name matches a column name.

--- lib/irdb/system_data_ext.rb
+++ lib/irdb/system_data_ext.rb
@@ -4,5 +4,11 @@ module System::Data
       table.columns.inject({}) { |h, col| h[col.to_s] = self[col]; h }
     end
     alias :to_h :to_hash
+    
+    def method_missing(symbol, *args, &block)
+      col_name = symbol.to_s
+      super unless table.columns.contains(col_name)
+      self[col_name]
+    end
   end
 end
--- test/data_row_ext_test.rb
+++ test/data_row_ext_test.rb
@@ -1,45 +1,50 @@
 require File.join(File.dirname(__FILE__), 'test_helper')
 
 class DataRowExtensions < Test::Unit::TestCase
-  context 'to_h' do
+  context 'A DataRow with no columns' do
     setup do
       @table = System::Data::DataTable.new
+      @row   = @table.new_row
+    end
+
+    should 'have to_hash alias' do
+      assert_equal @row.method(:to_hash), @row.method(:to_h)
+    end
+      
+    should 'convert to empty hash' do
+      assert_equal({}, @row.to_h)
+    end
+  end
+  
+  context 'A DataRow with columns' do
+    setup do
+      @table = System::Data::DataTable.new
+      @table.columns.add 'col1', String.to_clr_type
+      @table.columns.add 'col2', String.to_clr_type
+    
+      @row = @table.new_row
+      @row['col1'] = 'value1'
+      @row['col2'] = 'value2'
+    end
+      
+    should 'convert to hash with expected keys' do
+      assert @row.to_h.has_key?('col1')
+      assert @row.to_h.has_key?('col2')
     end
     
-    should "have to_hash alias" do
-      row = @table.new_row
-      assert_equal row.method(:to_hash), row.method(:to_h)
+    should 'convert to hash with expected values' do
+      assert_equal 'value1', @row.to_h['col1']
+      assert_equal 'value2', @row.to_h['col2']
     end
     
-    context 'when table has no columns' do
-      setup do
-        @result = @table.new_row.to_hash
-      end
-      
-      should 'returns empty hash' do
-        assert_equal @result, {}
+    should "raise NoMethodError when method name does not match a column name" do
+      assert_raise NoMethodError do
+        @row.col3
       end
     end
     
-    context 'when table has columns' do
-      setup do
-        @table.columns.add 'col1', String.to_clr_type
-        @table.columns.add 'col2', String.to_clr_type
-      
-        row = @table.new_row
-        row['col1'] = 'value1'
-        row['col2'] = 'value2'
-        @result = row.to_hash
-      end
-      
-      should 'call to_s on column to get key' do
-        assert @result.has_key?('col1')
-      end
-      
-      should 'keys should have expected values' do
-        assert_equal 'value1', @result['col1']
-        assert_equal 'value2', @result['col2']
-      end
+    should "return value of column if method name matches column name" do
+      assert_equal 'value1', @row.col1
     end
   end
 end
